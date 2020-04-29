e2baac5deb1d7a42406f6bd289b20e7af803965b
Sebastian Ripari <sebastiandripari@gmail.com>
2020-04-29 17:14:53 -0300

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
+  const linkElement = getByText(/Web Admin/i)
   expect(linkElement).toBeInTheDocument()
 })
