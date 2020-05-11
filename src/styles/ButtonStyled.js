e03eed05f8dedf422b50ea09fbc31d14b4fa37e6
Sebastian Ripari <sebastiandripari@gmail.com>
2020-05-11 02:29:29 -0300

    Button content center

--- src/styles/ButtonStyled.js
+++ src/styles/ButtonStyled.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 
 export const Button = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background-color: #61dafb;
   color: black;
   cursor: pointer;
