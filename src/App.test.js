9892530a78cf1a9808a911fc2268d2c46a805191
Sebastian Ripari <sebastiandripari@gmail.com>
2020-05-14 14:00:28 -0300

    Revert "Removed test"
    This reverts commit e467d5ba5042bfae97145ba2c3ad5047dcae8c07.

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/Web Admin/i)
+  expect(linkElement).toBeInTheDocument()
+})
