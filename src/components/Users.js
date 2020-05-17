99a259a3be0602643a367e8985383581033c9224
Sebastian Ripari <sebastiandripari@gmail.com>
2020-05-17 17:32:36 -0300

    Users Loader

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,10 +3,11 @@ import { useSelector } from 'react-redux'
 import { getUsers } from '../webapi'
 import { getToken } from '../stateapi/auth'
 import { UsersWrapper } from '../styles/UsersStyled'
+import CircleLoader from 'react-spinners/CircleLoader'
 
 const Users = () => {
   const token = useSelector(getToken)
-  const [users, changeUsers] = useState([])
+  const [users, changeUsers] = useState()
 
   useEffect(() => {
     getUsers(token)
@@ -22,30 +23,34 @@ const Users = () => {
   return (
     <UsersWrapper>
       <h2>Usuarios</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Nombre de Usuario</th>
-            <th>Nombre</th>
-            <th>Email</th>
-            <th>Telefono</th>
-          </tr>
-        </thead>
-        <tbody>
-          {(users || []).map(user => {
-            return (
-              <tr key={user.id}>
-                <td>{user.username}</td>
-                <td>
-                  {user.first_name} {user.last_name}
-                </td>
-                <td>{user.contact.email}</td>
-                <td>{user.contact.phone}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      {users ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre de Usuario</th>
+              <th>Nombre</th>
+              <th>Email</th>
+              <th>Telefono</th>
+            </tr>
+          </thead>
+          <tbody>
+            {(users || []).map(user => {
+              return (
+                <tr key={user.id}>
+                  <td>{user.username}</td>
+                  <td>
+                    {user.first_name} {user.last_name}
+                  </td>
+                  <td>{user.contact.email}</td>
+                  <td>{user.contact.phone}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      ) : (
+        <CircleLoader color='#61dafb' size={250} />
+      )}
     </UsersWrapper>
   )
 }
