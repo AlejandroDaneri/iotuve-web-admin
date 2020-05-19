09cdbc590b83088b0639e8bf7508e68806c16aa0
Sebastian <sebastiandripari@gmail.com>
2020-05-19 19:32:35 -0300

    Populating admin users

--- src/components/UsersAdmin.js
+++ src/components/UsersAdmin.js
@@ -1,25 +1,55 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { getToken } from '../stateapi/auth'
 import { getUsersAdmin } from '../webapi'
 
 const AdminUsers = () => {
   const token = useSelector(getToken)
+  const [users, changeUsers] = useState()
 
   useEffect(() => {
     getUsersAdmin(token)
       .then(response => {
         const { data } = response
-        console.error(data)
+        changeUsers(data)
       })
       .catch(_ => {
         console.error('Users Admin Get Error')
       })
   }, [token])
 
+  function parseTimestamp (timestamp) {
+    const date = new Date(timestamp)
+    return date.toUTCString()
+  }
+
   return (
     <div>
       <h2>Usuarios Admin</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre de Usuario</th>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Fecha de Creacion</th>
+          </tr>
+        </thead>
+        <tbody>
+          {(users || []).map(user => {
+            return (
+              <tr key={user.id}>
+                <td>{user.username}</td>
+                <td>
+                  {user.first_name} {user.last_name}
+                </td>
+                <td>{user.email}</td>
+                <td>{parseTimestamp(user.date_created)}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
