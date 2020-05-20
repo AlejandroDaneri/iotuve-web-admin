00ec167ca11b0048a4ed9b084fefb020711d601c
Sebastian Ripari <sebastiandripari@gmail.com>
2020-05-19 22:29:51 -0300

    Loader in Users Admin

--- src/components/UsersAdmin.js
+++ src/components/UsersAdmin.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import { getToken } from '../stateapi/auth'
 import { UsersAdminWrapper } from '../styles/UsersAdminStyled'
 import { getUsersAdmin } from '../webapi'
+import CircleLoader from 'react-spinners/CircleLoader'
 
 const AdminUsers = () => {
   const token = useSelector(getToken)
@@ -27,30 +28,34 @@ const AdminUsers = () => {
   return (
     <UsersAdminWrapper>
       <h2>Usuarios Admin</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Nombre de Usuario</th>
-            <th>Nombre</th>
-            <th>Email</th>
-            <th>Fecha de Creacion</th>
-          </tr>
-        </thead>
-        <tbody>
-          {(users || []).map(user => {
-            return (
-              <tr key={user.id}>
-                <td>{user.username}</td>
-                <td>
-                  {user.first_name} {user.last_name}
-                </td>
-                <td>{user.email}</td>
-                <td>{parseTimestamp(user.date_created)}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      {users ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre de Usuario</th>
+              <th>Nombre</th>
+              <th>Email</th>
+              <th>Fecha de Creacion</th>
+            </tr>
+          </thead>
+          <tbody>
+            {(users || []).map(user => {
+              return (
+                <tr key={user.id}>
+                  <td>{user.username}</td>
+                  <td>
+                    {user.first_name} {user.last_name}
+                  </td>
+                  <td>{user.email}</td>
+                  <td>{parseTimestamp(user.date_created)}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      ) : (
+        <CircleLoader color='#61dafb' size={250} />
+      )}
     </UsersAdminWrapper>
   )
 }
