c43cb5b4c06758a0829f9816e9a4abfd3bc966e3
Sebastian <sebastiandripari@gmail.com>
2020-06-01 14:22:04 -0300

    Move place of logout

--- src/styles/AppStyled.js
+++ src/styles/AppStyled.js
@@ -67,8 +67,8 @@ export const AppWrapper = styled.section`
     & > span {
       cursor: pointer;
       position: absolute;
-      bottom: 0;
-      left: 0;
+      top: 0;
+      right: 0;
       padding: 2%;
     }
   }
