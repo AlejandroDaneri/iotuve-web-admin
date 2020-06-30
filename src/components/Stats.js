72d146bc443270ab29324caaef0aeb814f15c4a5
Sebastian <sebastiandripari@gmail.com>
2020-06-30 10:54:47 -0300

    Showing Active Recovery & Active Sessions

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,18 +1,24 @@
 /* Import Libs */
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 /* Import Styled Components */
 import { StatsWrapper } from '../styles/StatsStyled'
 import { getStats } from '../webapi'
 
 const Stats = () => {
-  const startDate = '2020-05-30'
+  const startDate = '2020-04-30'
   const endDate = '2020-05-31'
+
+  const [activeRecovery, changeActiveRecovery] = useState()
+  const [activeSessions, changeActiveSessions] = useState()
 
   useEffect(() => {
     getStats(startDate, endDate)
-      .then(_ => {
-        console.error('Get Stats Successs')
+      .then(response => {
+        const { data } = response
+        console.error(data)
+        changeActiveRecovery(data.active_recovery)
+        changeActiveSessions(data.active_sessions)
       })
       .catch(_ => {
         console.error('Get Stats Error')
@@ -21,6 +27,8 @@ const Stats = () => {
   return (
     <StatsWrapper>
       <h2>Estadisticas</h2>
+      <div>Active Recovery: {activeRecovery}</div>
+      <div>Active Sessions: {activeSessions}</div>
     </StatsWrapper>
   )
 }
