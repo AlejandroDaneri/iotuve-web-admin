e7407ea0d8519d4e8268f36929a446bd1f32dc85
Sebastian <sebastiandripari@gmail.com>
2020-06-29 22:37:32 -0300

    Sidebar

--- src/App.js
+++ src/App.js
@@ -2,30 +2,11 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import {
-  Link,
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect
 } from 'react-router-dom'
-import {
-  Drawer,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText
-} from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import ShowChartIcon from '@material-ui/icons/ShowChart'
-import VideoLibraryIcon from '@material-ui/icons/VideoLibrary'
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
-import PersonIcon from '@material-ui/icons/Person'
-import DoneIcon from '@material-ui/icons/Done'
-import Divider from '@material-ui/core/Divider'
-import Tooltip from '@material-ui/core/Tooltip'
-
-/* Import Styled Components */
-import { AppWrapper } from './styles/AppStyled'
 
 /* Import Components */
 import Files from './components/Files'
@@ -37,10 +18,13 @@ import UserAdmin from './components/UserAdmin'
 import Users from './components/Users'
 import UsersAdmin from './components/UsersAdmin'
 import ChangePassword from './components/ChangePassword'
-import LogOut from './components/LogOut'
+import Sidebar from './components/Sidebar'
 
 /* Import StateApi */
 import { isAuthed, isAuthing } from './stateapi/auth'
+
+/* Import Styled Components */
+import { AppWrapper } from './styles/AppStyled'
 
 const PrivateRoute = ({ ...rest }) => {
   const authed = useSelector(isAuthed)
@@ -55,114 +39,14 @@ const PrivateRoute = ({ ...rest }) => {
   }
 }
 
-const useStyles = makeStyles(theme => ({
-  sidebar: { backgroundColor: '#1e3c5c', width: 'inherit' },
-  menuItem: { color: '#96a1af' }
-}))
-
 const App = () => {
   const authed = useSelector(isAuthed)
-  const classes = useStyles()
+
   return (
     <AppWrapper>
       <Router>
-        <div style={{ display: 'flex' }}>
-          {authed && (
-            <Drawer
-              style={{ width: '215px' }}
-              variant='persistent'
-              anchor='left'
-              open
-              classes={{ paper: classes.sidebar }}
-            >
-              <div style={{ placeSelf: 'end', padding: '10px' }}>
-                <LogOut />
-              </div>
-              <h1 className={classes.menuItem}>
-                <center>Web Admin</center>
-              </h1>
-              <Divider />
-              <Divider />
-              <List>
-                <Link to='/files'>
-                  <ListItem button>
-                    <ListItemIcon className={classes.menuItem}>
-                      <VideoLibraryIcon />
-                    </ListItemIcon>
-                    <Tooltip title='Ver los archivos que fueron subidos'>
-                      <ListItemText
-                        className={classes.menuItem}
-                        primary='Archivos'
-                      />
-                    </Tooltip>
-                  </ListItem>
-                </Link>
-              </List>
-              <List>
-                <Link to='/users'>
-                  <ListItem button>
-                    <ListItemIcon className={classes.menuItem}>
-                      <PersonIcon />
-                    </ListItemIcon>
-                    <Tooltip title='Ver los usarios que están registrados'>
-                      <ListItemText
-                        className={classes.menuItem}
-                        primary='Usuarios'
-                      />
-                    </Tooltip>
-                  </ListItem>
-                </Link>
-              </List>
-              <List>
-                <Link to='/users_admin'>
-                  <ListItem button>
-                    <ListItemIcon className={classes.menuItem}>
-                      <SupervisorAccountIcon />
-                    </ListItemIcon>
-                    <Tooltip title='Ver los admins que están registrados'>
-                      <ListItemText
-                        className={classes.menuItem}
-                        primary='Admins'
-                      />
-                    </Tooltip>
-                  </ListItem>
-                </Link>
-              </List>
-              <List>
-                <Link to='/stats'>
-                  <ListItem button>
-                    <ListItemIcon className={classes.menuItem}>
-                      <ShowChartIcon />
-                    </ListItemIcon>
-                    <Tooltip title='Ver estadísticas de los servidores'>
-                      <ListItemText
-                        className={classes.menuItem}
-                        primary='Estadisticas'
-                      />
-                    </Tooltip>
-                  </ListItem>
-                </Link>
-              </List>
-              <Divider />
-              <Divider />
-              <List>
-                <Link to='/health'>
-                  <ListItem button>
-                    <ListItemIcon className={classes.menuItem}>
-                      <DoneIcon />
-                    </ListItemIcon>
-                    <Tooltip title='Ver el estado actual de los servidores'>
-                      <ListItemText
-                        className={classes.menuItem}
-                        primary='Estado actual'
-                      />
-                    </Tooltip>
-                  </ListItem>
-                </Link>
-              </List>
-              <LogOut />
-            </Drawer>
-          )}
+        <div>
+          <Sidebar />
           <Switch>
             {!authed && <Route exact path='/' component={Login} />}
             <PrivateRoute path='/files' component={Files} />
---
+++ src/components/Sidebar.js
@@ -0,0 +1,134 @@
+/* Import Libs */
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from '@material-ui/core'
+import ShowChartIcon from '@material-ui/icons/ShowChart'
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary'
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
+import PersonIcon from '@material-ui/icons/Person'
+import DoneIcon from '@material-ui/icons/Done'
+import Divider from '@material-ui/core/Divider'
+import Tooltip from '@material-ui/core/Tooltip'
+import { makeStyles } from '@material-ui/core/styles'
+import { useSelector } from 'react-redux'
+
+/* Import Components */
+import LogOut from './LogOut'
+
+/* Import StateApi */
+import { isAuthed } from '../stateapi/auth'
+
+const useStyles = makeStyles(theme => ({
+  sidebar: { backgroundColor: '#1e3c5c', width: 'inherit' },
+  menuItem: { color: '#96a1af' }
+}))
+
+const Sidebar = () => {
+  const authed = useSelector(isAuthed)
+  const classes = useStyles()
+  return (
+    <div style={{ display: 'flex' }}>
+      {authed && (
+        <Drawer
+          style={{ width: '215px' }}
+          variant='persistent'
+          anchor='left'
+          open
+          classes={{ paper: classes.sidebar }}
+        >
+          <div style={{ placeSelf: 'end', padding: '10px' }}>
+            <LogOut />
+          </div>
+          <h1 className={classes.menuItem}>
+            <center>Web Admin</center>
+          </h1>
+          <Divider />
+          <Divider />
+          <List>
+            <Link to='/files'>
+              <ListItem button>
+                <ListItemIcon className={classes.menuItem}>
+                  <VideoLibraryIcon />
+                </ListItemIcon>
+                <Tooltip title='Ver los archivos que fueron subidos'>
+                  <ListItemText
+                    className={classes.menuItem}
+                    primary='Archivos'
+                  />
+                </Tooltip>
+              </ListItem>
+            </Link>
+          </List>
+          <List>
+            <Link to='/users'>
+              <ListItem button>
+                <ListItemIcon className={classes.menuItem}>
+                  <PersonIcon />
+                </ListItemIcon>
+                <Tooltip title='Ver los usarios que están registrados'>
+                  <ListItemText
+                    className={classes.menuItem}
+                    primary='Usuarios'
+                  />
+                </Tooltip>
+              </ListItem>
+            </Link>
+          </List>
+          <List>
+            <Link to='/users_admin'>
+              <ListItem button>
+                <ListItemIcon className={classes.menuItem}>
+                  <SupervisorAccountIcon />
+                </ListItemIcon>
+                <Tooltip title='Ver los admins que están registrados'>
+                  <ListItemText className={classes.menuItem} primary='Admins' />
+                </Tooltip>
+              </ListItem>
+            </Link>
+          </List>
+          <List>
+            <Link to='/stats'>
+              <ListItem button>
+                <ListItemIcon className={classes.menuItem}>
+                  <ShowChartIcon />
+                </ListItemIcon>
+                <Tooltip title='Ver estadísticas de los servidores'>
+                  <ListItemText
+                    className={classes.menuItem}
+                    primary='Estadisticas'
+                  />
+                </Tooltip>
+              </ListItem>
+            </Link>
+          </List>
+          <Divider />
+          <Divider />
+          <List>
+            <Link to='/health'>
+              <ListItem button>
+                <ListItemIcon className={classes.menuItem}>
+                  <DoneIcon />
+                </ListItemIcon>
+                <Tooltip title='Ver el estado actual de los servidores'>
+                  <ListItemText
+                    className={classes.menuItem}
+                    primary='Estado actual'
+                  />
+                </Tooltip>
+              </ListItem>
+            </Link>
+          </List>
+          <LogOut />
+        </Drawer>
+      )}
+    </div>
+  )
+}
+
+export default Sidebar
