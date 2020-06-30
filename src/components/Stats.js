8863d3612dd852532e390732dbbd79c331af8ff3
Sebastian <sebastiandripari@gmail.com>
2020-06-30 11:14:08 -0300

    Populating All Stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,11 +6,29 @@ import { StatsWrapper } from '../styles/StatsStyled'
 import { getStats } from '../webapi'
 
 const Stats = () => {
-  const startDate = '2020-04-30'
-  const endDate = '2020-05-31'
+  const startDate = '2020-04-29'
+  const endDate = '2020-04-30'
 
   const [activeRecovery, changeActiveRecovery] = useState()
   const [activeSessions, changeActiveSessions] = useState()
+
+  const [registeredAdminUsers, changeRegisteredAdminUsers] = useState()
+  const [
+    registeredAdminUsersActive,
+    changeRegisteredAdminUsersActive
+  ] = useState()
+  const [
+    registeredAdminUsersClosed,
+    changeRegisteredAdminUsersClosed
+  ] = useState()
+
+  const [registeredUsers, changeRegisteredUsers] = useState()
+  const [registeredUsersActive, changeRegisteredUsersActive] = useState()
+  const [registeredUsersClosed, changeRegisteredUsersClosed] = useState()
+  const [
+    registeredAdminUsersLoginService,
+    changeRegisteredAdminUsersLoginService
+  ] = useState()
 
   useEffect(() => {
     getStats(startDate, endDate)
@@ -19,6 +37,17 @@ const Stats = () => {
         console.error(data)
         changeActiveRecovery(data.active_recovery)
         changeActiveSessions(data.active_sessions)
+
+        changeRegisteredAdminUsers(data.registered_adminusers)
+        changeRegisteredAdminUsersActive(data.registered_adminusers_active)
+        changeRegisteredAdminUsersClosed(data.registered_adminusers_closed)
+
+        changeRegisteredUsers(data.registered_users)
+        changeRegisteredUsersActive(data.registered_users_active)
+        changeRegisteredUsersClosed(data.registered_users_closed)
+        changeRegisteredAdminUsersLoginService(
+          data.registered_users_login_service
+        )
       })
       .catch(_ => {
         console.error('Get Stats Error')
@@ -27,8 +56,24 @@ const Stats = () => {
   return (
     <StatsWrapper>
       <h2>Estadisticas</h2>
+
       <div>Active Recovery: {activeRecovery}</div>
       <div>Active Sessions: {activeSessions}</div>
+
+      <p />
+
+      <div>Registered Admin Users: {registeredAdminUsers}</div>
+      <div>Registered Admin Users Active: {registeredAdminUsersActive}</div>
+      <div>Registered Admin Users Closed: {registeredAdminUsersClosed}</div>
+
+      <p />
+
+      <div>Registered Users: {registeredUsers}</div>
+      <div>Registered Users Active: {registeredUsersActive}</div>
+      <div>Registered Users Closed: {registeredUsersClosed}</div>
+      <div>
+        Registered Users with Login Service: {registeredAdminUsersLoginService}
+      </div>
     </StatsWrapper>
   )
 }
