11d2cb01caea6756c68f28dda2250c7be37196b0
Sebastian <sebastiandripari@gmail.com>
2020-07-01 15:08:10 -0300

    Admin Active Sessions

--- src/components/UserAdmin/UserAdmin.js
+++ src/components/UserAdmin/UserAdmin.js
@@ -1,29 +1,17 @@
 /* Import Libs */
-import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import React from 'react'
 import { useParams } from 'react-router-dom'
-
-/* Import WebApi */
-import { getUserAdminSessions } from '../../webapi'
 
 /* Import Components */
 import Perfil from './Perfil'
 import ChangePassword from './ChangePassword'
+import ActiveSessions from './ActiveSessions'
 
 /* Import Styled Components */
 import { UserWrapper } from '../../styles/UserStyled'
 
 const UserAdmin = () => {
-  const dispatch = useDispatch()
   const { username } = useParams()
-  const [sessions, changeSessions] = useState()
-
-  useEffect(() => {
-    getUserAdminSessions(username).then(response => {
-      const { data } = response
-      changeSessions(data)
-    })
-  }, [username, dispatch])
 
   return (
     <UserWrapper>
@@ -31,20 +19,9 @@ const UserAdmin = () => {
 
       <div className='views'>
         <Perfil username={username} />
-
         <ChangePassword username={username} />
+        <ActiveSessions username={username} />
       </div>
-      <h2>Sessiones Activas</h2>
-      {sessions
-        ? sessions.map((session, index) => {
-            return (
-              <div key={index}>
-                <div>Creada: {session.date_created}</div>
-                <div>Expira: {session.expires}</div>
-              </div>
-            )
-          })
-        : 'Loading Sessions'}
     </UserWrapper>
   )
 }
