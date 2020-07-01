14fc445858d2021c8442d8805fff36d23fba3b67
Sebastian <sebastiandripari@gmail.com>
2020-07-01 15:00:20 -0300

    Improve Loader View

--- src/components/User/Perfil.js
+++ src/components/User/Perfil.js
@@ -62,106 +62,121 @@ const Perfil = ({ username }) => {
       .catch(_ => {})
   }
 
-  return loading ? (
-    <CircleLoader color={COLOR_PRIMARY} size={250} />
-  ) : (
+  return (
     <div className='perfil'>
       <div className='title'>
         <h3>Perfil</h3>
       </div>
 
-      <div className='row'>
-        <div className='field'>
-          Avatar
-          <br />
-          <a href={url}>
-            <img alt='Avatar thumb' width='100px' height='100px' src={url} />
-          </a>
-        </div>
+      {loading ? (
+        <CircleLoader color={COLOR_PRIMARY} size={250} />
+      ) : (
+        <>
+          <div className='row'>
+            <div className='field'>
+              Avatar
+              <br />
+              <a href={url}>
+                <img
+                  alt='Avatar thumb'
+                  width='100px'
+                  height='100px'
+                  src={url}
+                />
+              </a>
+            </div>
 
-        <div className='field'>
-          <div>Servicio de Login</div>
-          {loginService ? 'Si' : 'No'}
-        </div>
-      </div>
+            <div className='field'>
+              <div>Servicio de Login</div>
+              {loginService ? 'Si' : 'No'}
+            </div>
+          </div>
 
-      <p />
+          <p />
 
-      <div className='row'>
-        <div className='field'>
-          Mail
-          <br />
-          <input value={email} onChange={e => changeEmail(e.target.value)} />
-        </div>
+          <div className='row'>
+            <div className='field'>
+              Mail
+              <br />
+              <input
+                value={email}
+                onChange={e => changeEmail(e.target.value)}
+              />
+            </div>
 
-        <div className='field'>
-          Telefono
-          <br />
-          <input value={phone} onChange={e => changePhone(e.target.value)} />
-        </div>
-      </div>
+            <div className='field'>
+              Telefono
+              <br />
+              <input
+                value={phone}
+                onChange={e => changePhone(e.target.value)}
+              />
+            </div>
+          </div>
 
-      <p />
+          <p />
 
-      <div className='row'>
-        <div className='field'>
-          Nombre
-          <br />
-          <input
-            value={firstName}
-            onChange={e => changeFirstName(e.target.value)}
-          />
-        </div>
+          <div className='row'>
+            <div className='field'>
+              Nombre
+              <br />
+              <input
+                value={firstName}
+                onChange={e => changeFirstName(e.target.value)}
+              />
+            </div>
 
-        <div className='field'>
-          Apellido
-          <br />
-          <input
-            value={lastName}
-            onChange={e => changeLastName(e.target.value)}
-          />
-        </div>
-      </div>
+            <div className='field'>
+              Apellido
+              <br />
+              <input
+                value={lastName}
+                onChange={e => changeLastName(e.target.value)}
+              />
+            </div>
+          </div>
 
-      <p />
+          <p />
 
-      <div className='actions'>
-        <div className='action'>
-          <Button
-            href='/users'
-            variant='contained'
-            style={{ backgroundColor: COLOR_PRIMARY }}
+          <div className='actions'>
+            <div className='action'>
+              <Button
+                href='/users'
+                variant='contained'
+                style={{ backgroundColor: COLOR_PRIMARY }}
+              >
+                Cancelar{' '}
+              </Button>
+            </div>
+
+            <div className='action' onClick={() => save()}>
+              <Button
+                href='/users'
+                variant='contained'
+                style={{ backgroundColor: COLOR_PRIMARY }}
+              >
+                Guardar
+              </Button>
+            </div>
+          </div>
+
+          <Snackbar
+            open={success}
+            onClose={() => changeSuccess(false)}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+            autoHideDuration={6000}
           >
-            Cancelar{' '}
-          </Button>
-        </div>
-
-        <div className='action' onClick={() => save()}>
-          <Button
-            href='/users'
-            variant='contained'
-            style={{ backgroundColor: COLOR_PRIMARY }}
-          >
-            Guardar
-          </Button>
-        </div>
-      </div>
-
-      <Snackbar
-        open={success}
-        onClose={() => changeSuccess(false)}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-        autoHideDuration={6000}
-      >
-        <SnackbarContent
-          message='Usuario editado con exito'
-          style={{
-            color: 'black',
-            backgroundColor: COLOR_PRIMARY,
-            fontSize: '14px'
-          }}
-        />
-      </Snackbar>
+            <SnackbarContent
+              message='Usuario editado con exito'
+              style={{
+                color: 'black',
+                backgroundColor: COLOR_PRIMARY,
+                fontSize: '14px'
+              }}
+            />
+          </Snackbar>
+        </>
+      )}
     </div>
   )
 }
