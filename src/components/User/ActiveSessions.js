727e743c19cdeac5ae9b56ed98b8ec4def5d651a
Sebastian <sebastiandripari@gmail.com>
2020-07-01 14:56:51 -0300

    Show Sessions

--- src/components/User/ActiveSessions.js
+++ src/components/User/ActiveSessions.js
@@ -1,23 +1,44 @@
 /* Import Libs */
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
+import CircleLoader from 'react-spinners/CircleLoader'
 
 /* Import WebApi */
 import { getUserSessions } from '../../webapi'
 
+/* Import Constants */
+import { COLOR_PRIMARY } from '../../constants'
+
 const ActiveSessions = ({ username }) => {
   const dispatch = useDispatch()
+  const [loading, changeLoading] = useState(true)
+  const [sessions, changeSessions] = useState([])
 
   useEffect(() => {
     getUserSessions(username).then(response => {
       const { data } = response
       console.error(data)
+      changeSessions(data)
+      changeLoading(false)
     })
   }, [username, dispatch])
 
   return (
     <div className='active-sessions'>
       <h3>Sessiones Activas</h3>
+      {loading ? (
+        <CircleLoader color={COLOR_PRIMARY} size={250} />
+      ) : (
+        sessions.map((session, index) => {
+          return (
+            <div key={index}>
+              <div>Creada: {session.date_created}</div>
+              <div>Expira: {session.expires}</div>
+              <p />
+            </div>
+          )
+        })
+      )}
     </div>
   )
 }
