a53dcd5d1a9e8af3fbfbecb99da9050766a33611
Alejandro Daneri <alejandrodaneri07@gmail.com>
2020-07-10 19:26:52 -0300

    ready to merge new admin modal

--- src/components/User/UserAdmin/NewAdminModal.js
+++ src/components/User/UserAdmin/NewAdminModal.js
@@ -1,6 +1,6 @@
 import { createAdminUser } from '../../../webapi'
-import { Button } from '@material-ui/core'
-import { COLOR_PRIMARY } from '../../../constants'
+import { Button, Snackbar, SnackbarContent } from '@material-ui/core'
+import { COLOR_ERROR, COLOR_PRIMARY } from '../../../constants'
 import { StyledModal } from '../../../styles/ModalStyled'
 import React, { useState } from 'react'
 import { getToken } from '../../../stateapi/auth'
@@ -15,84 +15,128 @@ const NewAdminModal = ({ modalOpen, changeModalOpen }) => {
   const [email, setEmail] = useState('')
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
+  const [infoOpen, setInfoOpen] = useState(false)
+  const [error, setError] = useState(false)
+
+  function isDisabled () {
+    return !(
+      username &&
+      email &&
+      firstName &&
+      lastName &&
+      pwd &&
+      rePwd &&
+      pwd === rePwd
+    )
+  }
 
   function createAdmin () {
-    if (pwd && rePwd && pwd === rePwd) {
-      createAdminUser(token, {
-        username: username,
-        password: pwd,
-        first_name: firstName,
-        last_name: lastName,
-        email: email
+    createAdminUser(token, {
+      username: username,
+      password: pwd,
+      first_name: firstName,
+      last_name: lastName,
+      email: email
+    })
+      .then(r => {
+        setError(false)
+        setInfoOpen(true)
       })
-        .then(r => console.info('OK'))
-        .catch(e => console.error('ERROR'))
-    } else {
-      console.error('bad request')
-    }
+      .catch(e => {
+        setError(true)
+        setInfoOpen(true)
+      })
   }
 
   return (
-    <StyledModal
-      style={{ height: '35%' }}
-      isOpen={modalOpen}
-      onBackgroundClick={() => changeModalOpen(false)}
-      onEscapeKeydown={null}
-    >
-      <span>Ingrese los datos del nuevo admin</span>
-      <input
-        style={{ padding: '2px', margin: '2px' }}
-        placeholder='Nombres'
-        onChange={e => setFirstName(e.target.value)}
-      />
-      <input
-        style={{ padding: '2px', margin: '2px' }}
-        placeholder='Apellidos'
-        onChange={e => setLastName(e.target.value)}
-      />
-      <input
-        style={{ padding: '2px', margin: '2px' }}
-        placeholder='Email'
-        onChange={e => setEmail(e.target.value)}
-      />
-      <input
-        style={{ padding: '2px', margin: '2px' }}
-        placeholder='Nombre de usuario'
-        onChange={e => setUsername(e.target.value)}
-      />
-      <input
-        type='password'
-        style={{ padding: '2px', margin: '2px' }}
-        placeholder='Contraseña'
-        onChange={e => setPwd(e.target.value)}
-      />
-      <input
-        type='password'
-        style={{ padding: '2px', margin: '2px' }}
-        placeholder='Reingrese contraseña'
-        onChange={e => setRePwd(e.target.value)}
-      />
-
-      <div className='actions'>
-        <Button
-          variant='contained'
-          style={{ backgroundColor: COLOR_PRIMARY }}
-          onClick={() => changeModalOpen(false)}
-        >
-          Cancelar
-        </Button>
-        <Button
-          variant='contained'
-          style={{ backgroundColor: COLOR_PRIMARY }}
-          onClick={() => {
-            createAdmin()
-            changeModalOpen(false)
+    <>
+      <Snackbar
+        open={infoOpen}
+        onClose={() => setInfoOpen(false)}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        autoHideDuration={6000}
+      >
+        <SnackbarContent
+          message={error ? 'Error al crear el admin' : 'Admin creado con éxito'}
+          style={{
+            color: 'black',
+            backgroundColor: error ? COLOR_ERROR : COLOR_PRIMARY,
+            fontSize: '14px'
           }}
-        >
-          Aceptar
-        </Button>
-      </div>
-    </StyledModal>
+        />
+      </Snackbar>
+      <StyledModal
+        style={{ height: '35%' }}
+        isOpen={modalOpen}
+        onBackgroundClick={() => changeModalOpen(false)}
+        onEscapeKeydown={null}
+      >
+        <span>Ingrese los datos del nuevo admin</span>
+        <input
+          style={{ padding: '2px', margin: '2px' }}
+          placeholder='Nombres'
+          onChange={e => setFirstName(e.target.value)}
+        />
+        <input
+          style={{ padding: '2px', margin: '2px' }}
+          placeholder='Apellidos'
+          onChange={e => setLastName(e.target.value)}
+        />
+        <input
+          style={{ padding: '2px', margin: '2px' }}
+          placeholder='Email'
+          onChange={e => setEmail(e.target.value)}
+        />
+        <input
+          style={{ padding: '2px', margin: '2px' }}
+          placeholder='Nombre de usuario'
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          type='password'
+          style={{ padding: '2px', margin: '2px' }}
+          placeholder='Contraseña'
+          onChange={e => setPwd(e.target.value)}
+        />
+        <input
+          type='password'
+          style={{ padding: '2px', margin: '2px' }}
+          placeholder='Reingrese contraseña'
+          onChange={e => setRePwd(e.target.value)}
+        />
+
+        <div className='actions'>
+          <Button
+            variant='contained'
+            style={{ backgroundColor: COLOR_PRIMARY }}
+            onClick={() => changeModalOpen(false)}
+          >
+            Cancelar
+          </Button>
+
+          {isDisabled() ? (
+            <Button
+              variant='outlined'
+              style={{ borderColor: COLOR_PRIMARY, color: 'white' }}
+              disabled
+            >
+              Aceptar
+            </Button>
+          ) : (
+            <Button
+              variant='contained'
+              style={{ backgroundColor: COLOR_PRIMARY }}
+              onClick={() => {
+                createAdmin()
+                changeModalOpen(false)
+              }}
+            >
+              Aceptar
+            </Button>
+          )}
+        </div>
+      </StyledModal>
+    </>
   )
 }
 export default NewAdminModal
