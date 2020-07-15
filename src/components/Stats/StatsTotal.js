7abf57ba679b861dd0d730a67bf0180b4ed696ac
Sebastian <sebastiandripari@gmail.com>
2020-07-15 16:47:47 -0300

    Total done

--- src/components/Stats/StatsTotal.js
+++ src/components/Stats/StatsTotal.js
@@ -1,13 +1,18 @@
 /* Import Libs */
 import React, { useState, useEffect } from 'react'
 import { Pie } from 'react-chartjs-2'
+import CircleLoader from 'react-spinners/CircleLoader'
 
 /* Import WebApi */
 import { getStats } from '../../webapi'
 
+/* Import Constants */
+import { COLOR_PRIMARY } from '../../constants'
+
 const StatsTotal = () => {
   const [activeRecovery, changeActiveRecovery] = useState()
   const [activeSessions, changeActiveSessions] = useState()
+  const [loading, changeLoading] = useState(true)
 
   const [
     registeredAdminUsersActive,
@@ -41,8 +46,11 @@ const StatsTotal = () => {
         changeRegisteredUsersClosed(data.registered_users_closed)
 
         changeRegisteredUsersLoginService(data.registered_users_login_service)
+
+        changeLoading(false)
       })
       .catch(_ => {
+        changeLoading(false)
         console.error('Get Stats Error')
       })
   }
@@ -54,55 +62,60 @@ const StatsTotal = () => {
   return (
     <div className='stats'>
       <h2>Totales</h2>
-      <div className='numerical'>
-        <div className='stat'>
-          <h3>Sessiones & Links</h3>
-          <div>
-            Sesiones Activas: <b>{activeSessions}</b>
+      {loading ? (
+        <CircleLoader color={COLOR_PRIMARY} size={250} />
+      ) : (
+        <div className='numerical'>
+          <div className='stat'>
+            <h3>Sessiones & Links</h3>
+            <div>
+              Sesiones Activas: <b>{activeSessions}</b>
+            </div>
+            <div>
+              Links de Recuperar Contraseña Activos: <b>{activeRecovery}</b>
+            </div>
           </div>
-          <div>
-            Links de Recuperar Contraseña Activos: <b>{activeRecovery}</b>
-          </div>
-        </div>
 
-        <div className='stat'>
-          <h3>Usuarios Admin</h3>
-          <Pie
-            data={{
-              labels: ['Registrados Activos', 'Registrados Cerrados'],
-              datasets: [
-                {
-                  data: [
-                    registeredAdminUsersActive,
-                    registeredAdminUsersClosed
-                  ],
-                  backgroundColor: ['#36A2EB', '#FF6384'],
-                  hoverBackgroundColor: ['#36A2EB', '#FF6384']
-                }
-              ]
-            }}
-          />
-        </div>
+          <div className='stat'>
+            <h3>Usuarios Admin</h3>
+            <Pie
+              data={{
+                labels: ['Registrados Activos', 'Registrados Cerrados'],
+                datasets: [
+                  {
+                    data: [
+                      registeredAdminUsersActive,
+                      registeredAdminUsersClosed
+                    ],
+                    backgroundColor: ['#36A2EB', '#FF6384'],
+                    hoverBackgroundColor: ['#36A2EB', '#FF6384']
+                  }
+                ]
+              }}
+            />
+          </div>
 
-        <div className='stat'>
-          <h3>Usuarios</h3>
-          <Pie
-            data={{
-              labels: ['Registrados Activos', 'Registrados Cerrados'],
-              datasets: [
-                {
-                  data: [registeredUsersActive, registeredUsersClosed],
-                  backgroundColor: ['#36A2EB', '#FF6384'],
-                  hoverBackgroundColor: ['#36A2EB', '#FF6384']
-                }
-              ]
-            }}
-          />
-          <div>
-            Registrados con Login Service: <b>{registeredUsersLoginService}</b>
+          <div className='stat'>
+            <h3>Usuarios</h3>
+            <Pie
+              data={{
+                labels: ['Registrados Activos', 'Registrados Cerrados'],
+                datasets: [
+                  {
+                    data: [registeredUsersActive, registeredUsersClosed],
+                    backgroundColor: ['#36A2EB', '#FF6384'],
+                    hoverBackgroundColor: ['#36A2EB', '#FF6384']
+                  }
+                ]
+              }}
+            />
+            <div>
+              Registrados con Login Service:{' '}
+              <b>{registeredUsersLoginService}</b>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
