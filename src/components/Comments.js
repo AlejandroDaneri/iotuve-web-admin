256e71c0a0a66186917da08baf1e5fc3e40df5de
Alejandro Daneri <alejandrodaneri07@gmail.com>
2020-07-22 00:41:29 -0300

    add delete icon on comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,15 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import CircleLoader from 'react-spinners/CircleLoader'
-import { AUTH_LOGOUT, COLOR_PRIMARY } from '../constants'
+import { AUTH_LOGOUT, COLOR_ACTIONS, COLOR_PRIMARY } from '../constants'
 import { getComments } from '../webapi'
 import { useDispatch } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import { CommentsWrapper } from '../styles/CommentsStyled'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import { withStyles } from '@material-ui/core/styles'
+import TableCell from '@material-ui/core/TableCell'
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
+import Tooltip from '@material-ui/core/Tooltip'
+import DeleteModal from './Modal'
 
 const Comments = () => {
   const { video_id: videoID } = useParams()
+
   const [comments, setComments] = useState()
+  const [openModal, setOpenModal] = useState(false)
+
   const dispatch = useDispatch()
+
+  const StyledTableCell = withStyles(theme => ({
+    body: {
+      fontSize: 16,
+      color: COLOR_PRIMARY
+    },
+    root: {
+      borderBottom: 'none'
+    }
+  }))(TableCell)
+
+  const StyledTableRow = withStyles(theme => ({
+    root: {
+      '&:hover': {
+        backgroundColor: '#414855'
+      }
+    }
+  }))(TableRow)
 
   useEffect(() => {
     console.log(videoID)
@@ -35,38 +64,65 @@ const Comments = () => {
 
   return (
     <CommentsWrapper>
+      <DeleteModal
+        resource='comentario'
+        modalOpen={openModal}
+        changeModalOpen={setOpenModal}
+      />
       <h2>Comentarios</h2>
       {comments ? (
-        (comments || []).map(comment => {
-          return (
-            <>
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignSelf: 'flex-start',
-                  paddingLeft: '20px'
-                }}
-              >
-                <p>
-                  {comment.content}
-                  <br />
-                  <div style={{ display: 'flex', flexDirection: 'column' }}>
-                    <p style={{ fontSize: 'small', paddingLeft: '5px' }}>
-                      <Link
-                        to={`/user/${comment.user}`}
-                        style={{ color: 'white' }}
+        <Table>
+          <TableBody>
+            {(comments || []).map(comment => {
+              return (
+                <StyledTableRow key={comment.id}>
+                  <StyledTableCell>
+                    <>
+                      <div
+                        style={{
+                          display: 'flex',
+                          flexDirection: 'column',
+                          alignSelf: 'flex-start',
+                          paddingLeft: '5px'
+                        }}
                       >
-                        {comment.user}
-                      </Link>{' '}
-                      {parseTimestamp(comment.date_updated)}
-                    </p>
-                  </div>
-                </p>
-              </div>
-            </>
-          )
-        })
+                        <p>
+                          {comment.content}
+                          <br />
+                          <div
+                            style={{ display: 'flex', flexDirection: 'column' }}
+                          >
+                            <p
+                              style={{ fontSize: 'small', paddingLeft: '5px' }}
+                            >
+                              <Link
+                                to={`/user/${comment.user}`}
+                                style={{ color: 'white' }}
+                              >
+                                {comment.user}
+                              </Link>{' '}
+                              {parseTimestamp(comment.date_updated)}
+                            </p>
+                          </div>
+                        </p>
+                      </div>
+                    </>
+                  </StyledTableCell>
+                  <StyledTableCell>
+                    <Tooltip title='Eliminar comentario'>
+                      <DeleteForeverIcon
+                        style={{ color: COLOR_ACTIONS }}
+                        onClick={() => {
+                          setOpenModal(true)
+                        }}
+                      />
+                    </Tooltip>
+                  </StyledTableCell>
+                </StyledTableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
       ) : (
         <CircleLoader color={COLOR_PRIMARY} size={250} />
       )}
